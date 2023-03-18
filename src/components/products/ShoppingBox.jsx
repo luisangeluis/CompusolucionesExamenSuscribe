edc97094e795294b841a8ebba0577a9267175bf5
luisangeluis <luisangeluis@gmail.com>
2023-03-18 17:07:15 -0600

    Added styles to shopping box

--- src/components/products/ShoppingBox.jsx
+++ src/components/products/ShoppingBox.jsx
@@ -16,22 +16,29 @@ const ShoppingBox = ({ product }) => {
   }
 
   return (
-    <article className='card-footer border-0'>
+    <article className='card-footer border-1 bg-gray border border-2 border-dark-subtle'>
       <h3 className='subtitle-2'>Configura tus productos</h3>
       <div className="mb-3 d-flex justify-content-center align-items-start flex-column flex-lg-row">
         <h3 className='subtitle-2 text-start text-md-start px-1'>¿Cuantos quieres?</h3>
         <p className='subtitle-3 fw-bolder'>Disponible de 1 a 100</p>
       </div>
       <form action="">
-        <div className="mb-3">
-          <input type="number" className='form-control' onChange={handleOnChangeTotal} />
+        <div className=" input-group mb-3 ">
+          <span className='input-group-text bg-green text-white'>
+            <i className="fa-solid fa-cart-shopping mx-1 "></i>
+          </span>
+          <input type="number" className='form-control' onChange={handleOnChangeTotal}
+            placeholder="Numero de licencias" />
         </div>
         <div className="mb-3 text-end">
           Total: ${total.toFixed(2)}
         </div>
-        <button className='btn bg-green rounded-0 border border-1'>
-          Agregar al carrito
-        </button>
+        <div className='text-end'>
+          <button className='btn bg-green rounded-0 border border-1'>
+            Agregar al carrito
+          </button>
+        </div>
+
       </form>
     </article>
   )
